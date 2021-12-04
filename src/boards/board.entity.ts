c778b4f6598651426106e420f0d1ac3701990467
jinyul80 <jinyul80@gmail.com>
2021-12-04 23:41:03 +0900

    v 0.0.7
    - TypeORM  라이브러리 추가
    - Board entity 추가
    - Board repository  추가

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+import { ExecFileSyncOptionsWithBufferEncoding } from "child_process";
+import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { BoardStatus } from "./board.model";
+
+export class Board extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}

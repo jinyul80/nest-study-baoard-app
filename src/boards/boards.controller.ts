c2f86a08cae3a18c5611f1880d3d6e792f5626c8
jinyul80 <jinyul80@gmail.com>
2021-12-04 22:38:01 +0900

    v 0.0.5
    - ID로 게시물 찾는 기능 추가
    - 게시물 삭제 기능 추가
    - 게시물 업데이트 기능 추가
    - 게시물 생성 시 값 Validation 파이프 기능 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Board } from './board.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -12,8 +12,28 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
-    @Post('/')
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
     createBoard(@Body() createBoardDto: CreateBoardDto): Board {
         return this.boardsService.createBoard(createBoardDto);
     }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ): Board {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
+
 }

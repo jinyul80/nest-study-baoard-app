68318e560ba3e8250e575b6385b7070d5953e54e
jinyul80 <jinyul80@gmail.com>
2021-12-10 17:54:30 +0900

    v 0.1.4
    Eslint, Prettier  적용
    BoardController 단위 테스트 추가

--- nest-board-app/src/boards/boards.controller.spec.ts
+++ nest-board-app/src/boards/boards.controller.spec.ts
@@ -1,36 +1,59 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import exp from 'constants';
 import { BoardStatus } from './board-status.enum';
-import { Board } from './board.entity';
-import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 describe('BoardsController', () => {
   let controller: BoardsController;
 
-  const mockBoards = [{
-    id: 1,
-    title: "제목1",
-    description: "내용1",
-    status: BoardStatus.PUBLIC
-  }];
+  const mockBoards = [
+    {
+      id: 1,
+      title: '제목1',
+      description: '내용1',
+      status: BoardStatus.PUBLIC,
+    },
+    {
+      id: 2,
+      title: '제목2',
+      description: '내용2',
+      status: BoardStatus.PUBLIC,
+    },
+  ];
 
   const mockService = {
-    createBoard: jest.fn().mockImplementation(dto => Promise.resolve({
-      id: Date.now(),
-      title: dto.title,
-      description: dto.description,
-      status: BoardStatus.PUBLIC
-    }))
+    // morking 방법 1
+    // 서비스 생성하면서 morking
+    createBoard: jest.fn().mockImplementation((dto) =>
+      Promise.resolve({
+        id: Date.now(),
+        title: dto.title,
+        description: dto.description,
+        status: BoardStatus.PUBLIC,
+      }),
+    ),
+    deleteBoard: jest.fn().mockImplementation((id: number) => {
+      const found = mockBoards.find((board) => board.id === id);
+
+      if (!found) {
+        throw new NotFoundException(`Can't find Board with id ${id}`);
+      }
+    }),
+    getBoardById: jest.fn(),
+    getAllBoards: jest.fn(),
+    updateBoardStatus: jest.fn(),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BoardsController],
-      providers: [BoardsService]
-    }).overrideProvider(BoardsService).useValue(mockService).compile();
+      providers: [BoardsService],
+    })
+      .overrideProvider(BoardsService)
+      .useValue(mockService)
+      .compile();
 
     controller = module.get<BoardsController>(BoardsController);
   });
@@ -41,18 +64,90 @@ describe('BoardsController', () => {
 
   it('createBoard test', async () => {
     const dto = {
-      title: "제목1",
-      description: "내용1"
+      title: '제목1',
+      description: '내용1',
     };
 
     expect(await controller.createBoard(dto)).toEqual({
       id: expect.any(Number),
       title: dto.title,
       description: dto.description,
-      status: BoardStatus.PUBLIC
+      status: BoardStatus.PUBLIC,
     });
 
     expect(mockService.createBoard).toHaveBeenCalledWith(dto);
   });
 
+  it('deleteBoard test', async () => {
+    const id = 4;
+
+    try {
+      controller.deleteBoard(id);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NotFoundException);
+    }
+
+    expect(mockService.deleteBoard).toHaveBeenCalledWith(id);
+  });
+
+  it('getAllBoards test', async () => {
+    // morking 방법 2
+    // mockBoards 객체를 리턴한다고 가정
+    mockService.getAllBoards.mockResolvedValue(mockBoards);
+
+    const result = await controller.getAllBoards();
+
+    expect(mockService.getAllBoards).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(mockBoards);
+  });
+
+  describe('getBoardById test', () => {
+    it('게시물이 있는 경우', async () => {
+      const id = 1;
+
+      const mockBoard = mockBoards.find((board) => board.id === id);
+
+      // morking 방법 2
+      mockService.getBoardById.mockResolvedValue(mockBoard);
+
+      const result = await controller.getBoardById(id);
+
+      expect(mockService.getBoardById).toHaveBeenCalledWith(id);
+      expect(result).toEqual(mockBoard);
+    });
+
+    it('게시물이 없는 경우', async () => {
+      const id = 100;
+
+      // morking 방법 2
+      mockService.getBoardById.mockRejectedValue(new NotFoundException());
+
+      try {
+        await controller.getBoardById(id);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+      expect(mockService.getBoardById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  it('updateBoardStatus test', async () => {
+    const id = 1;
+
+    // morking 방법 2
+    // 메서드 재 형성
+    mockService.updateBoardStatus.mockImplementation((id, status) => {
+      const board = mockBoards.find((board) => board.id === id);
+      board.status = status;
+
+      return board;
+    });
+
+    const result = await controller.updateBoardStatus(id, BoardStatus.PRIVATE);
+
+    expect(result.id).toEqual(id);
+    expect(result.status).toEqual(BoardStatus.PRIVATE);
+
+    expect(mockService.updateBoardStatus).toHaveBeenCalledWith(id, BoardStatus.PRIVATE);
+  });
 });
--- nest-board-app/src/boards/boards.controller.ts
+++ nest-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
@@ -7,35 +18,34 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService) { }
+  constructor(private boardsService: BoardsService) {}
 
-    @Get()
-    getAllBoards(): Promise<Board[]> {
-        return this.boardsService.getAllBoards();
-    }
+  @Get()
+  getAllBoards(): Promise<Board[]> {
+    return this.boardsService.getAllBoards();
+  }
 
-    @Get(':id')
-    getBoardById(@Param('id') id: number): Promise<Board> {
-        return this.boardsService.getBoardById(id);
-    }
+  @Get(':id')
+  getBoardById(@Param('id') id: number): Promise<Board> {
+    return this.boardsService.getBoardById(id);
+  }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardsService.createBoard(createBoardDto);
-    }
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto);
+  }
 
-    @Delete(':id')
-    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.boardsService.deleteBoard(id);
-    }
+  @Delete(':id')
+  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardsService.deleteBoard(id);
+  }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: number,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ): Promise<Board> {
-        return this.boardsService.updateBoardStatus(id, status);
-    }
-
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: number,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ): Promise<Board> {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
 }

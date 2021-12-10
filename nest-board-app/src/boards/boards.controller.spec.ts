38790a49d372b0392fc7841c62b1b3f672c4059a
jinyul80 <jinyul80@gmail.com>
2021-12-10 09:00:41 +0900

    v 0.1.3
    BoardController 단위 테스트 추가중

--- nest-board-app/src/boards/boards.controller.spec.ts
+++ nest-board-app/src/boards/boards.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import exp from 'constants';
+import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
@@ -7,17 +10,49 @@ import { BoardsService } from './boards.service';
 describe('BoardsController', () => {
   let controller: BoardsController;
 
+  const mockBoards = [{
+    id: 1,
+    title: "제목1",
+    description: "내용1",
+    status: BoardStatus.PUBLIC
+  }];
+
+  const mockService = {
+    createBoard: jest.fn().mockImplementation(dto => Promise.resolve({
+      id: Date.now(),
+      title: dto.title,
+      description: dto.description,
+      status: BoardStatus.PUBLIC
+    }))
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BoardsController],
-      providers: [BoardsService, BoardRepository]
-    }).compile();
+      providers: [BoardsService]
+    }).overrideProvider(BoardsService).useValue(mockService).compile();
 
     controller = module.get<BoardsController>(BoardsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('createBoard test', async () => {
+    const dto = {
+      title: "제목1",
+      description: "내용1"
+    };
+
+    expect(await controller.createBoard(dto)).toEqual({
+      id: expect.any(Number),
+      title: dto.title,
+      description: dto.description,
+      status: BoardStatus.PUBLIC
+    });
+
+    expect(mockService.createBoard).toHaveBeenCalledWith(dto);
   });
 
 });

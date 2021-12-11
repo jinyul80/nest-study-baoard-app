e8fe50281291ea45106e9be6dcdff3ab26cbcfce
jinyul80 <jinyul80@gmail.com>
2021-12-12 00:59:00 +0900

    v 0.1.6
    Auth  모듈 추가
    - 유저 생성 기능 추가

--- nest-board-app/src/app.module.ts
+++ nest-board-app/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule
+    BoardsModule,
+    AuthModule
   ],
 })
 export class AppModule { }

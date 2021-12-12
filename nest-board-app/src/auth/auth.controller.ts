c9b9140de1e3ef6659de8921b5b7a48f2b9e9d36
jinyul80 <jinyul80@gmail.com>
2021-12-12 17:27:21 +0900

    v 0.1.10
    Auth  모듈
    - 로그인 기능 추가

--- nest-board-app/src/auth/auth.controller.ts
+++ nest-board-app/src/auth/auth.controller.ts
@@ -4,11 +4,15 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.authService.signUp(authCredentialsDto);
-    }
+  @Post('/signup')
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
 
+  @Post('/signIn')
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }

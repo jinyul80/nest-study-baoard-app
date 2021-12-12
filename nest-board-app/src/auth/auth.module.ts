924c3a9b7b480df6878707573a0290be15a9201b
jinyul80 <jinyul80@gmail.com>
2021-12-12 17:43:41 +0900

    v 0.1.11
    Auth 모듈
    - 로그인 성공시 JWT토큰 생성 기능 추가

--- nest-board-app/src/auth/auth.module.ts
+++ nest-board-app/src/auth/auth.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    JwtModule.register({
+      secret: config.get('jwt').secret,
+      signOptions: {
+        expiresIn: config.get('jwt').expriesIn,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
